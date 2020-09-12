a5b157ab21e2a80befe7131030b2f2a16c35bd9d
Tirumala_Rao_S <Tirumala_Rao_S@dell.com>
2020-09-12 21:05:35 +0530

    Page masthead

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2s3euDAxC3Um5tEHeanr2wY-spdqKKIo",
+  authDomain: "clone-2b951.firebaseapp.com",
+  databaseURL: "https://clone-2b951.firebaseio.com",
+  projectId: "clone-2b951",
+  storageBucket: "clone-2b951.appspot.com",
+  messagingSenderId: "815178432774",
+  appId: "1:815178432774:web:3b4fef833991c3a6139e87",
+  measurementId: "G-S4LV3GFD49"
+};
